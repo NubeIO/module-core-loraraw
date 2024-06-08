93c66e3448744598383d5f07e487d8ffdc859815
Tan Le <lmtan91@gmail.com>
2024-06-08 11:31:10 +0700

    Add support for rubix data decoding
    - Rewrite C++ decoder in rubix-data-encoding project
    - Add unittest rubix_test.go for decoder
    - Rewrite all JS/c++ decode unit tests
    - Integrate the rubix decoder with moduel
    - Fix typo
    
    Signed-off-by: Tan Le <lmtan91@gmail.com>

---
+++ decoder/rubix.go
@@ -0,0 +1,533 @@
+package decoder
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"math"
+	"unsafe"
+)
+
+const HEADER_BIT_COUNT = 6
+
+type DataType int
+type MetaDataKey int
+type BIT_TYPE uint64
+
+type MetaData struct {
+	dataType     DataType
+	lowValue     int
+	highValue    int
+	decimalPoint int
+	byteCount    int
+}
+
+type TRubix struct {
+	CommonValues
+	Temp      float32 `json:"temp-1"`
+	Rh        float32 `json:"rh-2"`
+	Lux       float32 `json:"lux-3"`
+	Movement  float32 `json:"movement-4"`
+	Count     float32 `json:"count-5"`
+	Digital   float32 `json:"digital-6"`
+	Voltage   float32 `json:"0-10v-7"`
+	Amplitude float32 `json:"4-20ma-8"`
+	Ohm       float32 `json:"ohm-9"`
+	Co2       float32 `json:"co2-10"`
+	BatVol    float32 `json:"battery-voltage-11"`
+	PushFreq  float32 `json:"push-frequency-12"`
+	Raw       float32 `json:"raw-13"`
+	Uo        float32 `json:"uo-14"`
+	Ui        float32 `json:"ui-15"`
+	Do        float32 `json:"do-16"`
+	Di        float32 `json:"di-17"`
+	FwVer     float32 `json:"firmware-version-18"`
+	HwVer     float32 `json:"hardware-version-19"`
+	Uint8     uint8   `json:"uint_8-20"`
+	Int8      int8    `json:"int_8-21"`
+	Uint16    uint16  `json:"uint_16-22"`
+	Int16     int16   `json:"int_16-23"`
+	Uint32    uint32  `json:"uint_32-24"`
+	Int32     int32   `json:"int_32-25"`
+	Uint64    uint64  `json:"uint_64-26"`
+	Int64     int64   `json:"int_64-27"`
+	Char      byte    `json:"char-28"`
+	Float1    float32 `json:"float-29"`
+	Float2    float32 `json:"float-30"`
+	Float3    float32 `json:"float-31"`
+}
+
+const (
+	MDK_TEMP             = 1
+	MDK_RH               = 2
+	MDK_LUX              = 3
+	MDK_MOVEMENT         = 4
+	MDK_COUNTER          = 5
+	MDK_DIGITAL          = 6
+	MDK_VOLTAGE_0_10     = 7
+	MDK_MILLIAMPS_4_20   = 8
+	MDK_OHM              = 10
+	MDK_CO2              = 11
+	MDK_BATTERY_VOLTAGE  = 12
+	MDK_PUSH_FREQUENCY   = 13
+	MDK_RAW              = 16
+	MDK_UO               = 17
+	MDK_UI               = 18
+	MDK_DO               = 19
+	MDK_DI               = 20
+	MDK_FIRMWARE_VERSION = 61
+	MDK_HARDWARE_VERSION = 62
+	MDK_UINT_8           = 30
+	MDK_INT_8            = 31
+	MDK_UINT_16          = 32
+	MDK_INT_16           = 33
+	MDK_UINT_32          = 34
+	MDK_INT_32           = 35
+	MDK_UINT_64          = 36
+	MDK_INT_64           = 37
+	MDK_BOOL             = 38
+	MDK_CHAR             = 39
+	MDK_FLOAT            = 40
+	MDK_DOUBLE           = 41
+	MDK_STRING           = 42
+)
+
+const (
+	FIXEDPOINT = 1
+	DATAPOINT  = 2
+)
+
+const (
+	SERIAL_DATA_MIN_SIZE         = 1
+	SERIAL_DATA_DATA_OFFSET_BITS = 8
+	SERIAL_DATA_DEFAULT_SETTINGS = 0
+)
+
+type SerialData struct {
+	Buffer     []byte
+	ReadBitPos int
+}
+
+func NewSerialData() *SerialData {
+	buffer := make([]byte, SERIAL_DATA_MIN_SIZE)
+	buffer[0] = SERIAL_DATA_DEFAULT_SETTINGS
+	return &SerialData{
+		Buffer:     buffer,
+		ReadBitPos: SERIAL_DATA_DATA_OFFSET_BITS,
+	}
+}
+
+func NewSerialDataWithBuffer(buffer []byte) *SerialData {
+	return &SerialData{
+		Buffer:     buffer,
+		ReadBitPos: SERIAL_DATA_DATA_OFFSET_BITS,
+	}
+}
+
+func canDecode(serialData *SerialData) bool {
+	return serialData.ReadBitPos < (len(serialData.Buffer)*8 - HEADER_BIT_COUNT)
+}
+
+func setPositionalData(serialData *SerialData, set bool) {
+	if set {
+		// Set the first bit in the first byte to 1
+		serialData.Buffer[0] |= 0x01
+	} else {
+		// Set the first bit in the first byte to 0
+		serialData.Buffer[0] &^= 0x01
+	}
+}
+
+func hasPositionalData(serialData *SerialData) bool {
+	return serialData.Buffer[0]&1 == 1
+}
+
+func getHeader(serialData *SerialData, position *uint8) MetaDataKey {
+
+	if hasPositionalData(serialData) {
+		positionVector, shiftPos, bytesRequired := getVector(serialData, 8, serialData.ReadBitPos)
+		*position = uint8(vectorToBits(positionVector, 8, shiftPos, bytesRequired))
+	}
+
+	typeVector, shiftPos, bytesRequired := getVector(serialData, HEADER_BIT_COUNT, serialData.ReadBitPos)
+	return MetaDataKey(vectorToBits(typeVector, HEADER_BIT_COUNT, shiftPos, bytesRequired))
+}
+
+func vectorToBits(dataVector []byte, bitCount, shiftPos, bytesRequired int) uint64 {
+	data64 := uint64(0)
+	shiftLeft := (bytesRequired*8 - bitCount - shiftPos)
+	for i := 0; i < len(dataVector)-1; i++ {
+		data64 |= uint64(dataVector[i])
+		if i != len(dataVector)-2 {
+			data64 <<= 8
+		}
+	}
+	// Shift forward and add last bits
+	data64 <<= (8 - shiftLeft)
+	data64 |= (uint64(dataVector[len(dataVector)-1]) >> shiftLeft)
+
+	// Mask out leading bits
+	if bitCount < 64 {
+		data64 &= (1 << uint(bitCount)) - 1
+	}
+
+	return data64
+}
+
+func getMetaData(header MetaDataKey) MetaData {
+	return serialMap[int(header)]
+}
+
+func getBitCount(low, high int, decimal int) int {
+	// Calculate the largest possible number
+	requiredSize := uint64(high-low) * uint64(math.Pow(10, float64(decimal)))
+
+	// Calculate number of bits required to fit number
+	bitCount := 1
+	for math.Pow(2, float64(bitCount)) < float64(requiredSize) {
+		bitCount++
+	}
+
+	return bitCount
+}
+
+func getVector(serialData *SerialData, bitCount, pos int) (dataVector []byte, shiftPos, bytesRequired int) {
+	// Calculate shift required from positioning of data
+	shiftFromPos := pos % 8
+	// Total bytes required for all data, can contain unwanted leading and trailing data
+	bytesRequired = bitCount / 8
+	if bitCount%8 != 0 {
+		bytesRequired++
+	}
+	// Checks if an extra byte is required
+	if bitCount+shiftFromPos > bytesRequired*8 {
+		bytesRequired++
+	}
+	// Build vector from relative bytes
+	byteLocation := pos / 8
+	dataVector = make([]byte, bytesRequired)
+	if byteLocation+bytesRequired > len(serialData.Buffer) {
+		dataVector[0] = 0 // Handle the case where requested data is beyond buffer size
+	} else {
+		for i := 0; i < bytesRequired; i++ {
+			dataVector[i] = serialData.Buffer[byteLocation+i]
+		}
+	}
+	// Update bit read position
+	serialData.ReadBitPos += bitCount
+
+	// Assign shiftPos and bytesRequired to the variables pointed by the pointers
+	shiftPos = shiftFromPos
+
+	return dataVector, shiftPos, bytesRequired
+}
+
+func decodeData(serialData *SerialData, header MetaDataKey, data interface{}) error {
+	if header == 0 {
+		return errors.New("invalid header")
+	}
+	metaData := getMetaData(header)
+	var shiftPos, bytesRequired, bitCount int
+	var dataBits BIT_TYPE
+	var dataVector []byte
+
+	switch metaData.dataType {
+	case FIXEDPOINT:
+		bitCount = getBitCount(metaData.lowValue, metaData.highValue, metaData.decimalPoint)
+		dataVector, shiftPos, bytesRequired = getVector(serialData, bitCount, serialData.ReadBitPos)
+		dataBits = BIT_TYPE(vectorToBits(dataVector, bitCount, shiftPos, bytesRequired))
+		switch v := data.(type) {
+		case *float32:
+			*v = float32(float64(dataBits)/math.Pow(10, float64(metaData.decimalPoint)) + float64(metaData.lowValue))
+		default:
+			return fmt.Errorf("unsupported data type: %T", data)
+		}
+	case DATAPOINT:
+		bitCount = metaData.byteCount * 8
+		dataVector, shiftPos, bytesRequired = getVector(serialData, bitCount, serialData.ReadBitPos)
+		dataBits = BIT_TYPE(vectorToBits(dataVector, bitCount, shiftPos, bytesRequired))
+		switch header {
+		case MDK_CHAR:
+			if v, ok := data.(*byte); ok {
+				*v = byte(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_CHAR: %T", data)
+			}
+		case MDK_UINT_8:
+			if v, ok := data.(*uint8); ok {
+				*v = uint8(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_UINT_8: %T", data)
+			}
+		case MDK_INT_8:
+			if v, ok := data.(*int8); ok {
+				*v = int8(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_INT_8: %T", data)
+			}
+		case MDK_UINT_16:
+			if v, ok := data.(*uint16); ok {
+				*v = uint16(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_UINT_16: %T", data)
+			}
+		case MDK_INT_16:
+			if v, ok := data.(*int16); ok {
+				*v = int16(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_INT_16: %T", data)
+			}
+		case MDK_UINT_32:
+			if v, ok := data.(*uint32); ok {
+				*v = uint32(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_UINT_32: %T", data)
+			}
+		case MDK_INT_32:
+			if v, ok := data.(*int32); ok {
+				*v = int32(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_INT_32: %T", data)
+			}
+		case MDK_UINT_64:
+			if v, ok := data.(*uint64); ok {
+				*v = uint64(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_UINT_64: %T", data)
+			}
+		case MDK_INT_64:
+			if v, ok := data.(*int64); ok {
+				*v = int64(dataBits)
+			} else {
+				return fmt.Errorf("invalid type for MDK_INT_64: %T", data)
+			}
+		case MDK_FLOAT:
+			if v, ok := data.(*float32); ok {
+				floatValue := *(*float32)(unsafe.Pointer(&dataBits))
+				*v = floatValue
+			} else {
+				return fmt.Errorf("invalid type for MDK_FLOAT: %T", data)
+			}
+		default:
+			return fmt.Errorf("unsupported header: %v", header)
+		}
+	default:
+		return fmt.Errorf("unsupported data type1: %v", metaData.dataType)
+	}
+	return nil
+}
+
+var serialMap = map[int]MetaData{
+	MDK_TEMP:             {FIXEDPOINT, -45, 120, 2, 0},
+	MDK_RH:               {FIXEDPOINT, 0, 100, 2, 0},
+	MDK_LUX:              {FIXEDPOINT, 0, 65534, 0, 0},
+	MDK_MOVEMENT:         {FIXEDPOINT, 0, 1, 0, 0},
+	MDK_COUNTER:          {FIXEDPOINT, 0, 1048576, 0, 0},
+	MDK_DIGITAL:          {FIXEDPOINT, 0, 1, 0, 0},
+	MDK_VOLTAGE_0_10:     {FIXEDPOINT, 0, 10, 2, 0},
+	MDK_MILLIAMPS_4_20:   {FIXEDPOINT, 4, 20, 2, 0},
+	MDK_OHM:              {FIXEDPOINT, 0, 1048576, 0, 0},
+	MDK_CO2:              {FIXEDPOINT, 0, 400, 0, 0},
+	MDK_BATTERY_VOLTAGE:  {FIXEDPOINT, 0, 6, 1, 0},
+	MDK_PUSH_FREQUENCY:   {FIXEDPOINT, 0, 2000, 0, 0},
+	MDK_RAW:              {FIXEDPOINT, 0, 1, 3, 0},
+	MDK_UO:               {FIXEDPOINT, 0, 1, 3, 0},
+	MDK_UI:               {FIXEDPOINT, 0, 1, 3, 0},
+	MDK_DO:               {FIXEDPOINT, 0, 1, 0, 0},
+	MDK_DI:               {FIXEDPOINT, 0, 1, 0, 0},
+	MDK_FIRMWARE_VERSION: {FIXEDPOINT, 0, 255, 0, 0},
+	MDK_HARDWARE_VERSION: {FIXEDPOINT, 0, 255, 0, 0},
+	MDK_UINT_8:           {DATAPOINT, 0, 0, 0, 1},
+	MDK_INT_8:            {DATAPOINT, 0, 0, 0, 1},
+	MDK_UINT_16:          {DATAPOINT, 0, 0, 0, 2},
+	MDK_INT_16:           {DATAPOINT, 0, 0, 0, 2},
+	MDK_UINT_32:          {DATAPOINT, 0, 0, 0, 4},
+	MDK_INT_32:           {DATAPOINT, 0, 0, 0, 4},
+	MDK_UINT_64:          {DATAPOINT, 0, 0, 0, 8},
+	MDK_INT_64:           {DATAPOINT, 0, 0, 0, 8},
+	MDK_BOOL:             {DATAPOINT, 0, 0, 0, 0},
+	MDK_CHAR:             {DATAPOINT, 0, 0, 0, 1},
+	MDK_FLOAT:            {DATAPOINT, 0, 0, 0, 4},
+	MDK_DOUBLE:           {DATAPOINT, 0, 0, 0, 8}}
+
+func DecodeRubix(data string, _ *LoRaDeviceDescription) (*CommonValues, interface{}) {
+	/*
+	 * Data Structure:
+	 * -------------------------------------------------------------------------------------------------------------------------------------
+	 * | 4 bytes address | 1 byte nonce | 1 byte length |            Payload           | 2 bytes RSSI | 2 bytes SNR |
+	 * -----------------------------------------------------------------------------------------------------
+	 * | data[0:4]       | data[4]      | data[5]       |       data[6:dataLen-4]      | data[dataLen-4:dataLen-2] | data[dataLen-2:dataLen]
+	 * -------------------------------------------------------------------------------------------------------------------------------------
+	 *
+	 * - 4 bytes address:              data[0:4]
+	 * - 1 byte nonce:                 data[4]
+	 * - 1 byte length field:          data[5]
+	 * - Payload:                      data[6:dataLen-4]
+	 * - 2 bytes RSSI:                 data[dataLen-4:dataLen-2]
+	 * - 2 bytes SNR:                  data[dataLen-2:dataLen]
+	 */
+
+	dataBytes, err := hex.DecodeString(data)
+	if err != nil {
+		fmt.Println("Error decoding hex string:", err)
+		return nil, nil
+	}
+
+	payloadLength := len(dataBytes) - (4 + 1 + 1 + 4)
+	payload := dataBytes[6 : 6+payloadLength]
+
+	serialData := NewSerialDataWithBuffer(payload)
+
+	var (
+		temperature float32
+		rh          float32
+		lux         float32
+		movement    float32
+		counter     float32
+		digital     float32
+		voltage     float32
+		amplitude   float32
+		ohm         float32
+		co2         float32
+		batVol      float32
+		pushFreq    float32
+		raw         float32
+		uo          float32
+		ui          float32
+		do          float32
+		di          float32
+		fwVer       float32
+		hwVer       float32
+		u8          uint8
+		i8          int8
+		u16         uint16
+		i16         int16
+		u32         uint32
+		i32         int32
+		u64         uint64
+		i64         int64
+		char        byte
+		fl1         float32
+		fl2         float32
+		fl3         float32
+	)
+	var float_count = 0
+	for canDecode(serialData) {
+		pos := uint8(0)
+		header := getHeader(serialData, &pos)
+		switch header {
+		case MDK_TEMP:
+			decodeData(serialData, header, &temperature)
+		case MDK_RH:
+			decodeData(serialData, header, &rh)
+		case MDK_LUX:
+			decodeData(serialData, header, &lux)
+		case MDK_MOVEMENT:
+			decodeData(serialData, header, &movement)
+		case MDK_COUNTER:
+			decodeData(serialData, header, &counter)
+		case MDK_DIGITAL:
+			decodeData(serialData, header, &digital)
+		case MDK_VOLTAGE_0_10:
+			decodeData(serialData, header, &voltage)
+		case MDK_MILLIAMPS_4_20:
+			decodeData(serialData, header, &amplitude)
+		case MDK_OHM:
+			decodeData(serialData, header, &ohm)
+		case MDK_CO2:
+			decodeData(serialData, header, &co2)
+		case MDK_BATTERY_VOLTAGE:
+			decodeData(serialData, header, &batVol)
+		case MDK_PUSH_FREQUENCY:
+			decodeData(serialData, header, &pushFreq)
+		case MDK_RAW:
+			decodeData(serialData, header, &raw)
+		case MDK_UO:
+			decodeData(serialData, header, &uo)
+		case MDK_UI:
+			decodeData(serialData, header, &ui)
+		case MDK_DO:
+			decodeData(serialData, header, &do)
+		case MDK_DI:
+			decodeData(serialData, header, &di)
+		case MDK_FIRMWARE_VERSION:
+			decodeData(serialData, header, &fwVer)
+		case MDK_HARDWARE_VERSION:
+			decodeData(serialData, header, &hwVer)
+		case MDK_UINT_8:
+			decodeData(serialData, header, &u8)
+		case MDK_INT_8:
+			decodeData(serialData, header, &i8)
+		case MDK_UINT_16:
+			decodeData(serialData, header, &u16)
+		case MDK_INT_16:
+			decodeData(serialData, header, &i16)
+		case MDK_UINT_32:
+			decodeData(serialData, header, &u32)
+		case MDK_INT_32:
+			decodeData(serialData, header, &i32)
+		case MDK_UINT_64:
+			decodeData(serialData, header, &u64)
+		case MDK_INT_64:
+			decodeData(serialData, header, &i64)
+		case MDK_CHAR:
+			decodeData(serialData, header, &char)
+		case MDK_FLOAT:
+			float_count++
+			if float_count == 1 {
+				decodeData(serialData, header, &fl1)
+			} else if float_count == 2 {
+				decodeData(serialData, header, &fl2)
+			} else if float_count == 3 {
+				decodeData(serialData, header, &fl3)
+			}
+
+		default:
+			fmt.Printf("Unknown header: %d\n", header)
+		}
+	}
+
+	v :=
+		TRubix{
+			Temp:      temperature,
+			Rh:        rh,
+			Lux:       lux,
+			Movement:  movement,
+			Count:     counter,
+			Digital:   digital,
+			Voltage:   voltage,
+			Amplitude: amplitude,
+			Ohm:       ohm,
+			Co2:       co2,
+			BatVol:    batVol,
+			PushFreq:  pushFreq,
+			Raw:       raw,
+			Uo:        uo,
+			Ui:        ui,
+			Do:        do,
+			Di:        di,
+			FwVer:     fwVer,
+			HwVer:     hwVer,
+			Uint8:     u8,
+			Int8:      i8,
+			Uint16:    u16,
+			Int16:     i16,
+			Uint32:    u32,
+			Int32:     i32,
+			Uint64:    u64,
+			Int64:     i64,
+			Char:      char,
+			Float1:    fl1,
+			Float2:    fl2,
+			Float3:    fl3,
+		}
+	return &v.CommonValues, v
+}
+
+func GetPointsStructRubix() interface{} {
+	return TRubix{}
+}
+
+func CheckPayloadLengthRubix(data string) bool {
+	return true
+}
---
+++ decoder/rubix_test.go
@@ -0,0 +1,287 @@
+package decoder
+
+import (
+	"reflect"
+	"testing"
+)
+
+func testHeader(t *testing.T, serialData *SerialData, expectedHeader MetaDataKey) {
+	var pos uint8
+	header := getHeader(serialData, &pos)
+	if header != expectedHeader {
+		t.Fatalf("Expected header %v, but got %v", expectedHeader, header)
+	}
+}
+
+func testFloat(t *testing.T, serialData *SerialData, expected_header MetaDataKey, expected float32) {
+	var value float32
+	err := decodeData(serialData, expected_header, &value)
+	if err != nil {
+		t.Fatalf("Failed to decode data: %v", err)
+	}
+	if value != expected {
+		t.Fatalf("Expected float value %v, but got %v", expected, value)
+	}
+}
+
+func testDecodeFloat(t *testing.T, serialData *SerialData, expected float32, h MetaDataKey) {
+	if !canDecode(serialData) {
+		t.Fatalf("Expected canDecode to return true, but it returned false")
+	}
+
+	testHeader(t, serialData, h)
+	testFloat(t, serialData, h, expected)
+}
+
+func testUint(t *testing.T, serialData *SerialData, expectedHeader MetaDataKey, expected interface{}) {
+	value := reflect.New(reflect.TypeOf(expected)).Interface()
+	err := decodeData(serialData, expectedHeader, value)
+	if err != nil {
+		t.Fatalf("Failed to decode data: %v", err)
+	}
+
+	if !reflect.DeepEqual(reflect.ValueOf(value).Elem().Interface(), expected) {
+		t.Fatalf("Expected value %v, but got %v", expected, reflect.ValueOf(value).Elem().Interface())
+	}
+}
+
+func testDecodeUint(t *testing.T, serialData *SerialData, expected interface{}, h MetaDataKey) {
+	if !canDecode(serialData) {
+		t.Fatalf("Expected canDecode to return true, but it returned false")
+	}
+
+	testHeader(t, serialData, h)
+	testUint(t, serialData, h, expected)
+}
+
+func testInt(t *testing.T, serialData *SerialData, expectedHeader MetaDataKey, expected interface{}) {
+	value := reflect.New(reflect.TypeOf(expected)).Interface()
+	err := decodeData(serialData, expectedHeader, value)
+	if err != nil {
+		t.Fatalf("Failed to decode data: %v", err)
+	}
+
+	if !reflect.DeepEqual(reflect.ValueOf(value).Elem().Interface(), expected) {
+		t.Fatalf("Expected value %v, but got %v", expected, reflect.ValueOf(value).Elem().Interface())
+	}
+}
+
+func testDecodeInt(t *testing.T, serialData *SerialData, expected interface{}, h MetaDataKey) {
+	if !canDecode(serialData) {
+		t.Fatalf("Expected canDecode to return true, but it returned false")
+	}
+
+	testHeader(t, serialData, h)
+	testInt(t, serialData, h, expected)
+}
+
+func testChar(t *testing.T, serialData *SerialData, expectedHeader MetaDataKey, expected byte) {
+	var value byte
+	err := decodeData(serialData, expectedHeader, &value)
+	if err != nil {
+		t.Fatalf("Failed to decode data: %v", err)
+	}
+	if value != expected {
+		t.Fatalf("Expected char value %v, but got %v", expected, value)
+	}
+}
+
+func testDecodeChar(t *testing.T, serialData *SerialData, expected byte, h MetaDataKey) {
+	if !canDecode(serialData) {
+		t.Fatalf("Expected canDecode to return true, but it returned false")
+	}
+
+	testHeader(t, serialData, h)
+	testChar(t, serialData, h, expected)
+}
+
+func expectTrue(t *testing.T, serialData *SerialData) {
+	if !canDecode(serialData) {
+		t.Fatalf("Expected canDecode to return true, but it returned false")
+	}
+}
+
+func expectFalse(t *testing.T, serialData *SerialData) {
+	if canDecode(serialData) {
+		t.Fatalf("Expected canDecode to return false, but it returned true")
+	}
+}
+
+func requireFalse(t *testing.T, condition bool, msg string) {
+	if condition {
+		t.Fatalf("REQUIRE_FALSE failed: %s", msg)
+	}
+}
+
+func TestDecodefromStm32Encode(t *testing.T) {
+	pl := []byte{0, 5, 92, 240, 74, 217, 134, 205, 44, 36, 83, 13, 63,
+		26, 62, 240, 68, 192, 41, 178, 7, 11, 166, 152, 233, 160,
+		61, 13, 227, 209, 111, 139, 30, 1, 123, 253, 229, 7, 236,
+		31, 0, 120, 5, 224, 39, 192, 222, 15, 124, 125, 227, 247,
+		223, 223, 128, 121, 65, 251, 8, 1, 144, 97, 249, 191, 136,
+		0, 19, 136, 6, 63, 255, 177, 223, 249, 0, 0, 0, 3,
+		185, 172, 160, 0, 101, 255, 255, 255, 241, 25, 77, 127, 255,
+		148, 0, 0, 0, 0, 0, 0, 0, 2, 95, 255, 255, 255,
+		255, 255, 255, 255, 249, 216, 104, 63, 128, 0, 0, 161, 12,
+		72, 125, 246, 132, 53, 227, 141, 90, 16, 233, 170, 168, 0}
+
+	serialData := SerialData{
+		Buffer:     pl,
+		ReadBitPos: 8,
+	}
+
+	Require(t, canDecode(&serialData))
+	testDecodeFloat(t, &serialData, 66.66, MDK_TEMP)
+	testDecodeFloat(t, &serialData, 55.55, MDK_RH)
+	testDecodeFloat(t, &serialData, 26262, MDK_LUX)
+	testDecodeFloat(t, &serialData, 1, MDK_MOVEMENT)
+	testDecodeFloat(t, &serialData, 199999, MDK_COUNTER)
+	testDecodeFloat(t, &serialData, 1, MDK_DIGITAL)
+	testDecodeFloat(t, &serialData, 8.88, MDK_VOLTAGE_0_10)
+	testDecodeFloat(t, &serialData, 16.16, MDK_MILLIAMPS_4_20)
+	testDecodeFloat(t, &serialData, 444444, MDK_OHM)
+	testDecodeFloat(t, &serialData, 333, MDK_CO2)
+	testDecodeFloat(t, &serialData, 2.9, MDK_BATTERY_VOLTAGE)
+	testDecodeFloat(t, &serialData, 15, MDK_PUSH_FREQUENCY)
+	testDecodeFloat(t, &serialData, 0.888, MDK_RAW)
+	testDecodeFloat(t, &serialData, 22, MDK_FIRMWARE_VERSION)
+	testDecodeFloat(t, &serialData, 44, MDK_HARDWARE_VERSION)
+	testDecodeUint(t, &serialData, (uint8)(1), MDK_UINT_8)
+	testDecodeUint(t, &serialData, (uint8)(0xFF), MDK_UINT_8)
+	testDecodeUint(t, &serialData, (uint8)(80), MDK_UINT_8)
+	testDecodeInt(t, &serialData, int8(-80), MDK_INT_8)
+	testDecodeInt(t, &serialData, int8(0), MDK_INT_8)
+	testDecodeUint(t, &serialData, (uint8)(1), MDK_UINT_8)
+	testDecodeUint(t, &serialData, (uint8)(2), MDK_UINT_8)
+	testDecodeInt(t, &serialData, int8(3), MDK_INT_8)
+	testDecodeUint(t, &serialData, (uint8)(15), MDK_UINT_8)
+	testDecodeInt(t, &serialData, int8(31), MDK_INT_8)
+	testDecodeUint(t, &serialData, (uint8)(63), MDK_UINT_8)
+	testDecodeInt(t, &serialData, int8(127), MDK_INT_8)
+	testDecodeInt(t, &serialData, int8(-128), MDK_INT_8)
+	testDecodeUint(t, &serialData, (uint8)(80), MDK_UINT_8)
+	testDecodeInt(t, &serialData, int8(-80), MDK_INT_8)
+	testDecodeUint(t, &serialData, (uint16)(1601), MDK_UINT_16)
+	testDecodeInt(t, &serialData, int16(-1601), MDK_INT_16)
+	testDecodeUint(t, &serialData, uint32(320001), MDK_UINT_32)
+	testDecodeInt(t, &serialData, int32(-320001), MDK_INT_32)
+	testDecodeUint(t, &serialData, uint64(64000000001), MDK_UINT_64)
+	testDecodeInt(t, &serialData, int64(-64000000001), MDK_INT_64)
+	testDecodeInt(t, &serialData, int64(0), MDK_INT_64)
+	testDecodeInt(t, &serialData, int64(-1), MDK_INT_64)
+	testDecodeChar(t, &serialData, byte('a'), MDK_CHAR)
+	testDecodeFloat(t, &serialData, (1.0), MDK_FLOAT)
+	testDecodeFloat(t, &serialData, 146.123, MDK_FLOAT)
+	testDecodeFloat(t, &serialData, 222.222, MDK_FLOAT)
+	testDecodeFloat(t, &serialData, 333.333, MDK_FLOAT)
+	requireFalse(t, canDecode(&serialData), "Should not decode")
+}
+
+func Require(t *testing.T, condition bool) {
+	if !condition {
+		t.Fatalf("REQUIRE failed:")
+	}
+}
+
+func TestSerialDataSettings(t *testing.T) {
+	sd := NewSerialData()
+	Require(t, len(sd.Buffer) == 1)
+	Require(t, hasPositionalData(sd) == false)
+	setPositionalData(sd, true)
+	Require(t, sd.Buffer[0] == 1)
+	Require(t, hasPositionalData(sd) == true)
+	setPositionalData(sd, false)
+	Require(t, sd.Buffer[0] == 0)
+	Require(t, hasPositionalData(sd) == false)
+}
+
+func TestSerialDataRaw(t *testing.T) {
+	pl := []byte{0, 64, 100, 64, 200, 65, 44, 65, 144, 65, 244, 64, 100,
+		64, 200, 65, 44, 65, 144, 65, 244, 66, 88, 26, 52}
+	serialData := NewSerialDataWithBuffer(pl)
+
+	Require(t, canDecode(serialData))
+
+	testDecodeFloat(t, serialData, 0.1, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.2, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.3, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.4, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.5, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.1, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.2, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.3, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.4, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.5, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.6, MDK_RAW)
+	testDecodeFloat(t, serialData, 1, MDK_DIGITAL)
+	testDecodeFloat(t, serialData, 1, MDK_DIGITAL)
+
+	requireFalse(t, canDecode(serialData), "Should not decode")
+}
+
+func TestDecodeDataType(t *testing.T) {
+	pl := []byte{0, 121, 65, 251, 8, 1, 144, 97, 249, 191, 136, 0, 19, 136, 6,
+		63, 255, 177, 223, 249, 216, 104, 63, 143, 190, 119, 162, 254,
+		62, 249, 222, 132, 71, 156, 5, 32}
+	serialData := NewSerialDataWithBuffer(pl)
+
+	Require(t, canDecode(serialData))
+
+	testDecodeUint(t, serialData, uint8(80), MDK_UINT_8)
+	testDecodeInt(t, serialData, int8(-80), MDK_INT_8)
+	testDecodeUint(t, serialData, uint16(1601), MDK_UINT_16)
+	testDecodeInt(t, serialData, int16(-1601), MDK_INT_16)
+	testDecodeUint(t, serialData, uint32(320001), MDK_UINT_32)
+	testDecodeInt(t, serialData, int32(-320001), MDK_INT_32)
+	testDecodeChar(t, serialData, byte('a'), MDK_CHAR)
+	testDecodeFloat(t, serialData, 1.123, MDK_FLOAT)
+	testDecodeFloat(t, serialData, -1.123, MDK_FLOAT)
+	testDecodeFloat(t, serialData, 999.005, MDK_FLOAT)
+	requireFalse(t, canDecode(serialData), "Should not decode")
+}
+
+func TestPositional(t *testing.T) {
+	pl := []byte{1, 1, 64, 100, 3, 65, 44, 5, 65, 244, 7, 66, 188}
+	serialData := NewSerialDataWithBuffer(pl)
+
+	Require(t, canDecode(serialData))
+
+	testDecodeFloat(t, serialData, 0.1, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.3, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.5, MDK_RAW)
+	testDecodeFloat(t, serialData, 0.7, MDK_RAW)
+	requireFalse(t, canDecode(serialData), "Should not decode")
+}
+
+func TestSerialDataFull(t *testing.T) {
+	pl := []byte{0, 5, 92, 240, 74, 217, 134, 205, 44, 36, 83, 13, 63, 26,
+		62, 240, 68, 192, 41, 178, 7, 11, 166, 152, 233, 160, 61, 13,
+		225, 17, 145, 35, 33, 50, 159, 69, 190, 44}
+	serialData := NewSerialDataWithBuffer(pl)
+
+	Require(t, canDecode(serialData))
+
+	testDecodeFloat(t, serialData, 66.66, MDK_TEMP)
+	testDecodeFloat(t, serialData, 55.55, MDK_RH)
+	testDecodeFloat(t, serialData, 26262, MDK_LUX)
+	testDecodeFloat(t, serialData, 1, MDK_MOVEMENT)
+	testDecodeFloat(t, serialData, 199999, MDK_COUNTER)
+	testDecodeFloat(t, serialData, 1, MDK_DIGITAL)
+	testDecodeFloat(t, serialData, 8.88, MDK_VOLTAGE_0_10)
+	testDecodeFloat(t, serialData, 16.16, MDK_MILLIAMPS_4_20)
+	testDecodeFloat(t, serialData, 444444, MDK_OHM)
+	testDecodeFloat(t, serialData, 333, MDK_CO2)
+	testDecodeFloat(t, serialData, 2.9, MDK_BATTERY_VOLTAGE)
+	testDecodeFloat(t, serialData, 15, MDK_PUSH_FREQUENCY)
+	testDecodeFloat(t, serialData, 0.888, MDK_RAW)
+
+	testDecodeFloat(t, serialData, 0.1, MDK_UO)
+	testDecodeFloat(t, serialData, 0.2, MDK_UI)
+	testDecodeFloat(t, serialData, 0, MDK_DO)
+	testDecodeFloat(t, serialData, 1, MDK_DI)
+
+	testDecodeFloat(t, serialData, 22, MDK_FIRMWARE_VERSION)
+	testDecodeFloat(t, serialData, 44, MDK_HARDWARE_VERSION)
+
+	requireFalse(t, canDecode(serialData), "Should not decode")
+}
