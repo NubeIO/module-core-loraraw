ee0500156342d7ddbeb21af7651ade87e98a2f28
Tan Le <lmtan91@gmail.com>
2024-06-14 15:40:27 +0700

    add arm build files

---
+++ build.bash
@@ -0,0 +1,8 @@
+#/bin/bash
+
+MODULE_NAME="module-core-loraraw"
+
+docker build -t module-builder -f Dockerfile.module --build-arg="MODULE_NAME=$MODULE_NAME" .
+docker run -d --name module-builder module-builder:latest
+docker container cp module-builder:/app/$MODULE_NAME .
+docker rm -f module-builder
