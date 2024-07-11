ecf560b3231086a61f78821951933409ed5165f6
Tan Le <lmtan91@gmail.com>
2024-07-11 09:48:11 +0700

    Integrate rubix decoding with latest point update refactor
    Signed-off-by: Tan Le <lmtan91@gmail.com>

--- decoder/rubix.go
+++ decoder/rubix.go
@@ -9,6 +9,7 @@ import (
 	"unsafe"
 
 	"github.com/NubeIO/nubeio-rubix-lib-models-go/model"
+	log "github.com/sirupsen/logrus"
 )
 
 const HEADER_BIT_COUNT = 6
@@ -71,6 +72,40 @@ const (
 	SERIAL_DATA_DEFAULT_SETTINGS = 0
 )
 
+const (
+	TempField           = "temp"
+	RHField             = "rh"
+	LuxField            = "lux"
+	MovementField       = "movement"
+	CounterField        = "count"
+	DigitalField        = "digital"
+	VoltageField        = "0-10v"
+	MilliampsField      = "4-20ma"
+	OhmField            = "ohm"
+	CO2Field            = "co2"
+	BatteryVoltageField = "battery-voltage"
+	PushFrequencyField  = "push-frequency"
+	RawField            = "raw"
+	UOField             = "uo"
+	UIField             = "ui"
+	DOField             = "do"
+	DIField             = "di"
+	FwVersionField      = "firmware-version"
+	HwVersionField      = "hardware-version"
+	BoolField           = "bool"
+	CharField           = "char"
+	UInt8Field          = "uint_8"
+	Int8Field           = "int_8"
+	UInt16Field         = "uint_16"
+	Int16Field          = "int_16"
+	UInt32Field         = "uint_32"
+	Int32Field          = "int_32"
+	UInt64Field         = "uint_64"
+	Int64Field          = "int_64"
+	FloatField          = "float"
+	DoubleField         = "double"
+)
+
 type SerialData struct {
 	Buffer     []byte
 	ReadBitPos int
@@ -111,12 +146,10 @@ func hasPositionalData(serialData *SerialData) bool {
 }
 
 func getHeader(serialData *SerialData, position *uint8) MetaDataKey {
-
 	if hasPositionalData(serialData) {
 		positionVector, shiftPos, bytesRequired := getVector(serialData, 8, serialData.ReadBitPos)
 		*position = uint8(vectorToBits(positionVector, 8, shiftPos, bytesRequired))
 	}
-
 	typeVector, shiftPos, bytesRequired := getVector(serialData, HEADER_BIT_COUNT, serialData.ReadBitPos)
 	return MetaDataKey(vectorToBits(typeVector, HEADER_BIT_COUNT, shiftPos, bytesRequired))
 }
@@ -313,16 +346,23 @@ var serialMap = map[int]MetaData{
 	MDK_INT_32:           {DATAPOINT, 0, 0, 0, 4},
 	MDK_UINT_64:          {DATAPOINT, 0, 0, 0, 8},
 	MDK_INT_64:           {DATAPOINT, 0, 0, 0, 8},
-	MDK_BOOL:             {DATAPOINT, 0, 0, 0, 0},
+	MDK_BOOL:             {FIXEDPOINT, 0, 1, 0, 0},
 	MDK_CHAR:             {DATAPOINT, 0, 0, 0, 1},
 	MDK_FLOAT:            {DATAPOINT, 0, 0, 0, 4},
 	MDK_DOUBLE:           {DATAPOINT, 0, 0, 0, 8}}
+
+func generateFieldName(baseName string, hasPosition bool, pos *uint8) string {
+	if !hasPosition {
+		*pos++
+	}
+	return baseName + "-" + strconv.Itoa(int(*pos))
+}
 
 func DecodeRubix(data string, devDesc *LoRaDeviceDescription, device *model.Device) error {
 	/*
 	 * Data Structure:
 	 * -----------------------------------------------------------------------------------------------------------------------------------------------------
-	 * | 4 bytes address | 1 byte opts  | 1 byte nonce  | 1 byte length |            Payload           | 2 bytes RSSI              |   2 bytes SNR         |
+	 * | 4 bytes address | 1 byte opts  | 1 byte nonce  | 1 byte length |            Payload           | 1 bytes RSSI              |   1 bytes SNR         |
 	 * -----------------------------------------------------------------------------------------------------------------------------------------------------
 	 * | data[0:3]       | data[4]      | data[5]       | data[6]       |     data[7:dataLen-4]        | data[dataLen-4:dataLen-2] | data[dataLen-2:dataLen]
 	 * -----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -331,95 +371,170 @@ func DecodeRubix(data string, devDesc *LoRaDeviceDescription, device *model.Devi
 	 * - 1 byte opts:                  data[4]
 	 * - 1 byte nonce:                 data[5]
 	 * - 1 byte length field:          data[6]
-	 * - Payload:                      data[7:dataLen-4]
-	 * - 2 bytes RSSI:                 data[dataLen-4:dataLen-2]
-	 * - 2 bytes SNR:                  data[dataLen-2:dataLen]
+	 * - Payload:                      data[7:dataLen-2]
+	 * - 1 bytes RSSI:                 data[dataLen-2:dataLen-1]
+	 * - 1 bytes SNR:                  data[dataLen-1:dataLen]
 	 */
+
+	var (
+		temperature float32
+		rh          float32
+		lux         float32
+		movement    float32
+		counter     float32
+		digital     float32
+		voltage     float32
+		amplitude   float32
+		ohm         float32
+		co2         float32
+		batVol      float32
+		pushFreq    float32
+		raw         float32
+		uo          float32
+		ui          float32
+		do          float32
+		di          float32
+		fwVer       float32
+		hwVer       float32
+		u8          uint8
+		i8          int8
+		u16         uint16
+		i16         int16
+		u32         uint32
+		i32         int32
+		u64         uint64
+		i64         int64
+		char        byte
+		fl1         float32
+		b1          float32
+	)
+
+	commonValues := &CommonValues{}
+	decodeCommonValues(commonValues, data, devDesc.Model)
+	if commonValues == nil {
+		return errors.New("invalid common values")
+	}
+
+	updateDeviceFault(commonValues.ID, commonValues.Sensor, device.UUID, commonValues.Rssi)
+
+	err := updateDevicePoint(RssiField, float64(commonValues.Rssi), device)
+	if err != nil {
+		return err
+	}
+
+	err = updateDevicePoint(SnrField, float64(commonValues.Snr), device)
+	if err != nil {
+		return err
+	}
 
 	dataBytes, err := hex.DecodeString(data)
 	if err != nil {
-		fmt.Println("Error decoding hex string:", err)
+		log.Errorf("Error decoding hex string:", err)
 		return err
 	}
-	payloadLength := len(dataBytes) - (4 + 1 + 1 + 1 + 4)
+	payloadLength := len(dataBytes) - (4 + 1 + 1 + 1 + 2)
 	payload := dataBytes[7 : 7+payloadLength]
 
 	serialData := NewSerialDataWithBuffer(payload)
 
-	var float_count = 0
-
+	hasPos := hasPositionalData(serialData)
+	var position uint8 = 0
 	for canDecode(serialData) {
-		pos := uint8(0)
-		header := getHeader(serialData, &pos)
+		header := getHeader(serialData, &position)
 		switch header {
 		case MDK_TEMP:
 			decodeData(serialData, header, &temperature)
+			_ = updateDevicePoint(generateFieldName(TempField, hasPos, &position), float64(temperature), device)
 		case MDK_RH:
 			decodeData(serialData, header, &rh)
+			_ = updateDevicePoint(generateFieldName(RHField, hasPos, &position), float64(rh), device)
 		case MDK_LUX:
 			decodeData(serialData, header, &lux)
+			_ = updateDevicePoint(generateFieldName(LuxField, hasPos, &position), float64(lux), device)
 		case MDK_MOVEMENT:
 			decodeData(serialData, header, &movement)
+			_ = updateDevicePoint(generateFieldName(MovementField, hasPos, &position), float64(movement), device)
 		case MDK_COUNTER:
 			decodeData(serialData, header, &counter)
+			_ = updateDevicePoint(generateFieldName(CounterField, hasPos, &position), float64(counter), device)
 		case MDK_DIGITAL:
 			decodeData(serialData, header, &digital)
+			_ = updateDevicePoint(generateFieldName(DigitalField, hasPos, &position), float64(digital), device)
 		case MDK_VOLTAGE_0_10:
 			decodeData(serialData, header, &voltage)
+			_ = updateDevicePoint(generateFieldName(VoltageField, hasPos, &position), float64(voltage), device)
 		case MDK_MILLIAMPS_4_20:
 			decodeData(serialData, header, &amplitude)
+			_ = updateDevicePoint(generateFieldName(MilliampsField, hasPos, &position), float64(amplitude), device)
 		case MDK_OHM:
 			decodeData(serialData, header, &ohm)
+			_ = updateDevicePoint(generateFieldName(OhmField, hasPos, &position), float64(ohm), device)
 		case MDK_CO2:
 			decodeData(serialData, header, &co2)
+			_ = updateDevicePoint(generateFieldName(CO2Field, hasPos, &position), float64(co2), device)
 		case MDK_BATTERY_VOLTAGE:
 			decodeData(serialData, header, &batVol)
+			_ = updateDevicePoint(generateFieldName(BatteryVoltageField, hasPos, &position), float64(batVol), device)
 		case MDK_PUSH_FREQUENCY:
 			decodeData(serialData, header, &pushFreq)
+			_ = updateDevicePoint(generateFieldName(PushFrequencyField, hasPos, &position), float64(pushFreq), device)
 		case MDK_RAW:
 			decodeData(serialData, header, &raw)
+			_ = updateDevicePoint(generateFieldName(RawField, hasPos, &position), float64(raw), device)
 		case MDK_UO:
 			decodeData(serialData, header, &uo)
+			_ = updateDevicePoint(generateFieldName(UOField, hasPos, &position), float64(uo), device)
 		case MDK_UI:
 			decodeData(serialData, header, &ui)
+			_ = updateDevicePoint(generateFieldName(UIField, hasPos, &position), float64(ui), device)
 		case MDK_DO:
 			decodeData(serialData, header, &do)
+			_ = updateDevicePoint(generateFieldName(DOField, hasPos, &position), float64(do), device)
 		case MDK_DI:
 			decodeData(serialData, header, &di)
+			_ = updateDevicePoint(generateFieldName(DIField, hasPos, &position), float64(di), device)
 		case MDK_FIRMWARE_VERSION:
 			decodeData(serialData, header, &fwVer)
+			_ = updateDevicePoint(generateFieldName(FwVersionField, hasPos, &position), float64(fwVer), device)
 		case MDK_HARDWARE_VERSION:
 			decodeData(serialData, header, &hwVer)
+			_ = updateDevicePoint(generateFieldName(HwVersionField, hasPos, &position), float64(hwVer), device)
 		case MDK_UINT_8:
 			decodeData(serialData, header, &u8)
+			_ = updateDevicePoint(generateFieldName(UInt8Field, hasPos, &position), float64(u8), device)
 		case MDK_INT_8:
 			decodeData(serialData, header, &i8)
+			_ = updateDevicePoint(generateFieldName(Int8Field, hasPos, &position), float64(i8), device)
 		case MDK_UINT_16:
 			decodeData(serialData, header, &u16)
+			_ = updateDevicePoint(generateFieldName(UInt16Field, hasPos, &position), float64(u16), device)
 		case MDK_INT_16:
 			decodeData(serialData, header, &i16)
+			_ = updateDevicePoint(generateFieldName(Int16Field, hasPos, &position), float64(i16), device)
 		case MDK_UINT_32:
 			decodeData(serialData, header, &u32)
+			_ = updateDevicePoint(generateFieldName(UInt32Field, hasPos, &position), float64(u32), device)
 		case MDK_INT_32:
 			decodeData(serialData, header, &i32)
+			_ = updateDevicePoint(generateFieldName(Int32Field, hasPos, &position), float64(i32), device)
 		case MDK_UINT_64:
 			decodeData(serialData, header, &u64)
+			_ = updateDevicePoint(generateFieldName(UInt64Field, hasPos, &position), float64(u64), device)
 		case MDK_INT_64:
 			decodeData(serialData, header, &i64)
+			_ = updateDevicePoint(generateFieldName(Int64Field, hasPos, &position), float64(i64), device)
 		case MDK_CHAR:
 			decodeData(serialData, header, &char)
+			_ = updateDevicePoint(generateFieldName(CharField, hasPos, &position), float64(char), device)
 		case MDK_FLOAT:
-			float_count++
-			if float_count == 1 {
-				decodeData(serialData, header, &fl1)
-			} else if float_count == 2 {
-				decodeData(serialData, header, &fl2)
-			} else if float_count == 3 {
-				decodeData(serialData, header, &fl3)
-			}
-
+			decodeData(serialData, header, &fl1)
+			_ = updateDevicePoint(generateFieldName(FloatField, hasPos, &position), float64(fl1), device)
+		case MDK_BOOL:
+			decodeData(serialData, header, &b1)
+			_ = updateDevicePoint(generateFieldName(BoolField, hasPos, &position), float64(b1), device)
 		default:
-			fmt.Printf("Unknown header: %d\n", header)
+			log.Errorf("Unknown header: %d", header)
+
 		}
 	}
 
