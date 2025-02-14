60eff72f0ec50f5a507ee413c76e4bd9069774aa
Tan Le <lmtan91@gmail.com>
2025-02-14 15:56:23 +0700

    Support lora as923
    Signed-off-by: Tan Le <lmtan91@gmail.com>

--- pkg/config.go
+++ pkg/config.go
@@ -3,11 +3,13 @@ package pkg
 import (
 	"encoding/hex"
 	"errors"
+	"os"
+	"strings"
+	"time"
+
 	"github.com/NubeIO/module-core-loraraw/logger"
 	"github.com/go-yaml/yaml"
 	log "github.com/sirupsen/logrus"
-	"strings"
-	"time"
 )
 
 type Config struct {
@@ -15,6 +17,7 @@ type Config struct {
 	LogLevel           string        `yaml:"log_level"`
 	DefaultKey         string        `yaml:"default_key" type:"secret"`
 	DecryptionDisabled bool          `yaml:"decryption_disabled"`
+	LoRaFrequencyPlan  string        `yaml:"lora_freq_plan"`
 }
 
 const DefaultDeviceKey = "0301021604050f07e6095a0b0c12630f"
@@ -25,6 +28,7 @@ func (m *Module) DefaultConfig() *Config {
 		LogLevel:           "ERROR",
 		DefaultKey:         DefaultDeviceKey,
 		DecryptionDisabled: false,
+		LoRaFrequencyPlan:  "AU915",
 	}
 }
 
@@ -47,6 +51,20 @@ func (m *Module) ValidateAndSetConfig(config []byte) ([]byte, error) {
 		newConfig.DefaultKey = DefaultDeviceKey
 	} else if len(keyBytes) != 16 {
 		return nil, errors.New("invalid default key: must be exactly 16 bytes")
+	}
+
+	freqConfig := map[string]string{
+		"lora_frequency_plan": newConfig.LoRaFrequencyPlan,
+	}
+
+	configYaml, err := yaml.Marshal(freqConfig)
+	if err != nil {
+		return nil, errors.New("error marshaling frequency plan config")
+	}
+
+	err = os.WriteFile("/data/lora_frequency_plan.yml", configYaml, 0644)
+	if err != nil {
+		return nil, errors.New("error writing frequency plan file")
 	}
 
 	newConfValid, err := yaml.Marshal(newConfig)
